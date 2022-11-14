72444f9b0988712e31dc604fb0d110076b166eac
jankithummar <jankithummar2001@gmail.com>
2022-11-14 17:18:03 +0530

    first

---
+++ lib/appbar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Appbar extends StatefulWidget {
+  const Appbar({Key? key}) : super(key: key);
+
+  @override
+  State<Appbar> createState() => _AppbarState();
+}
+
+class _AppbarState extends State<Appbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.amber,
+    );
+  }
+}
